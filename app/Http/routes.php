1582177aca23b76ce2257afaeb3300ff9620da71
jaumei <jaumei@gmail.com>
2015-03-09 18:31:59 +0100

    pr02

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,9 +11,11 @@
 |
 */
 
-Route::get('/', 'WelcomeController@index');
+Route::get('/', 'MyController@index');
 
 Route::get('home', 'HomeController@index');
+
+Route::get('alumnes', 'AlumneController@index');
 
 Route::controllers([
 	'auth' => 'Auth\AuthController',
---
+++ resources/views/myview.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: atapis
+ * Date: 26/02/15
+ * Time: 18:22
+ */
